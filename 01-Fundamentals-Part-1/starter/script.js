16c7ec1bb490d09c042d9f5db59c8425e03eb8d6
Roger O'Donnell <71945647+Roger-ODonnell@users.noreply.github.com>
2025-02-26 20:54:42 +0000

    Added some dataType's to script.js

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -4,7 +4,7 @@
 //   alert(js);
 // }
 
-console.log(81 + 41 - 10);
+/*console.log(81 + 41 - 10);
 
 let firstName = "Matilda";
 let first = "Roger";
@@ -13,4 +13,26 @@ let firstNamePerson = "Roger";
 console.log(firstName);
 console.log(first);
 console.log(firstNamePerson);
-console.log(firstName);
+console.log(firstName);*/
+
+let JavaScriptIsFun = true;
+console.log(JavaScriptIsFun);
+
+console.log(typeof true);
+console.log(typeof JavaScriptIsFun);
+console.log(typeof 23);
+console.log(typeof "JavaScriptIsFun");
+
+JavaScriptIsFun = "YES!";
+
+console.log(typeof JavaScriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
