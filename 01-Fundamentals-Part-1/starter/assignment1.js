78c711568f302747e138dcfc789b00eab6c0d2ae
Roger O'Donnell <71945647+Roger-ODonnell@users.noreply.github.com>
2025-02-26 20:57:20 +0000

    Added typeof checks to assignment one variables

--- 01-Fundamentals-Part-1/starter/assignment1.js
+++ 01-Fundamentals-Part-1/starter/assignment1.js
@@ -5,3 +5,12 @@ let population = "5.3 Million";
 console.log(country);
 console.log(continent);
 console.log(population);
+
+let isIsland = true;
+let language;
+
+console.log(typeof country);
+console.log(typeof continent);
+console.log(typeof population);
+console.log(typeof isIsland);
+console.log(typeof language);
