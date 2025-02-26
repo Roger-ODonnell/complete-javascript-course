a75b4591ea94e680e72aa8d7dc18f6aaa77d1656
Roger O'Donnell <71945647+Roger-ODonnell@users.noreply.github.com>
2025-02-26 20:23:05 +0000

    Added variable examples

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,7 +1,16 @@
-let js = "Js is fun";
+// let js = "Js is fun";
 
-if (js === "Js is fun") {
-  alert(js);
-}
+// if (js === "Js is fun") {
+//   alert(js);
+// }
 
 console.log(81 + 41 - 10);
+
+let firstName = "Matilda";
+let first = "Roger";
+let firstNamePerson = "Roger";
+
+console.log(firstName);
+console.log(first);
+console.log(firstNamePerson);
+console.log(firstName);
