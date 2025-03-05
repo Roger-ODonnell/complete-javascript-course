68d5e03aec6e2796aeaa7691547d478af173b497
Roger O'Donnell <71945647+Roger-ODonnell@users.noreply.github.com>
2025-03-05 22:30:04 +0000

    Added if block, else if and else block examples to script.js

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -101,3 +101,18 @@ if (age == 18) console.log("You are now an adult 😊(loose)");
 //== type coercion
 //=== no type coercion
 //always use strict
+
+let favouriteNumber = Number(prompt("What is your favourite number?"));
+
+if (favouriteNumber === 23){
+    console.log("Cool! 23 is an amazing number");
+}else if (favouriteNumber === 7){
+    console.log("7 is also a cool number");
+}else if (favouriteNumber === 9){
+    console.log("9 is also a cool number!")
+}
+else{
+    console.log("Number is not 23 or 7 or 9");
+}
+
+if (favouriteNumber !== 23) console.log("Why not 23?");
