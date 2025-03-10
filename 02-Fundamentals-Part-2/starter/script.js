0fd93a27a8ef306687f21606f7d7db4e9401e566
Roger O'Donnell <71945647+Roger-ODonnell@users.noreply.github.com>
2025-03-10 20:06:43 +0000

    Added function declartion vs function expression examples in script.js in foundations part 2

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -6,19 +6,34 @@
 // if (passTest) hasDriversLicense = true;
 // if(hasDriversLicense) console.log('I can drive 😊')
 
-function logger(log){
-    console.log(log);
-}
+// function logger(log){
+//     console.log(log);
+// }
 
-function fruitProcessor (apples, oranges){
-    const juice = `juice with ${apples} apples and ${oranges} oranges`;
-    return juice
-}
+// function fruitProcessor (apples, oranges){
+//     const juice = `juice with ${apples} apples and ${oranges} oranges`;
+//     return juice
+// }
 
-const appleJuice = fruitProcessor(5,0);
-console.log(appleJuice);
+// const appleJuice = fruitProcessor(5,0);
+// console.log(appleJuice);
 
-const appleOrangeJuice = fruitProcessor(2, 4);
-console.log(appleOrangeJuice);
+// const appleOrangeJuice = fruitProcessor(2, 4);
+// console.log(appleOrangeJuice);
 
-const num = Number('23');
+// const num = Number('23');
+
+//###function declaration
+// function calcAge1 (birthYear){
+//     return 2037 - birthYear;
+// }
+
+// //###function expression
+// const calcAge2 = function(birthYear){
+//     return 2037 - birthYear;
+// }
+
+// const age1 = calcAge1(1991);
+// const age2 = calcAge2(1991);
+// console.log(age1, age2);
+
