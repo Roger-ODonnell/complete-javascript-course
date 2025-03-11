0373bdc77501c85b7afac2982591d390dbf3f7f1
Roger O'Donnell <71945647+Roger-ODonnell@users.noreply.github.com>
2025-03-11 22:34:53 +0000

    completed challenge 2 in foundations part 2

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -127,3 +127,102 @@ function log(log) {
 // }
 
 // checkWinner(scoreDolphins, scoreKoalas);
+
+// const friend1 = "Michael";
+// const friend2 = "Steven";
+// const friend3 = "Peter";
+
+// const friends = ["Michael", "Steven", "Peter"];
+// console.log(friends);
+
+// const years = new Array(1991, 1984, 2008, 2020);
+
+// console.log(friends[0]);
+// console.log(friends[2]);
+// console.log(friends.length);
+// console.log(friends[friends.length - 1]);
+
+// friends[2] = "Jane";
+// console.log(friends);
+
+// const firstName = "Roger";
+// const roger = [firstName, "Odonnell", 2037 - 1991, "teacher", friends];
+// console.log(roger);
+
+// //exercise
+// const calcAge = function (birthYear) {
+//   return 2037 - birthYear;
+// };
+
+// const years = [19990, 1967, 2002, 2010, 2018];
+
+// const age1 = calcAge(years[0]);
+// const age2 = calcAge(years[1]);
+// const age3 = calcAge(years[years.length - 1]);
+// console.log(age1, age2, age3);
+
+// const ages = [calcAge(years[0]), calcAge(years[1])];
+// calcAge[years.length - 1];
+// console.log(ages);
+
+// const friends = ["Michael", "Steven", "Peter"];
+
+// //Add elements
+// const newLength = friends.push("Jay");
+// console.log(friends);
+// console.log(newLength);
+
+// friends.unshift("John");
+// console.log(friends);
+
+// //Remove elements
+// friends.pop(); //Removes last element of array
+// const popped = friends.pop(); //Removes last element of array
+// console.log(popped);
+// console.log(friends);
+
+// friends.shift();
+// console.log(friends);
+
+// friends.push(23);
+// console.log(friends.indexOf("Steven"));
+// console.log(friends.includes("Steven"));
+// console.log(friends.includes(23));
+
+// friends.push("Peter");
+
+// if (friends.includes("Peter")) {
+//   console.log("You have a friend named peter");
+//   console.log(friends);
+// }
+
+// Steven wants you to improve his tip calculator, using the same rules as before — tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
+
+// Your tasks:
+
+// Write a function calcTip that takes any bill value as an input and returns the corresponding tip, calculated based on the rules above (you can check out the code from the first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100.
+
+// And now let's use arrays! So, create an array called bills containing the test data below.
+
+// Create an array called tips containing the tip value for each bill, calculated from the function you created before.
+
+// BONUS: Create an array totals containing the total values, so the bill + tip.
+
+// TEST DATA: 125, 555, and 44.
+
+const tipCal = (bill) => {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
+
+const bills = [125, 555, 44];
+
+const tips = [tipCal(bills[0]), tipCal(bills[1]), tipCal(bills[2])];
+
+const totals = [
+  tipCal(bills[0]) + bills[0],
+  tipCal(bills[1]) + bills[1],
+  tipCal(bills[2]) + bills[2],
+];
+
+console.log(bills);
+console.log(totals);
