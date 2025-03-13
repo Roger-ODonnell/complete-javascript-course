5e206ad3a9d90c70c85769ab55810f47ffd44ea1
Roger O'Donnell <71945647+Roger-ODonnell@users.noreply.github.com>
2025-03-13 08:33:11 +0000

    Completed foundations part 2: Challenge 3: OBJECTS

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -224,43 +224,134 @@ function log(log) {
 // console.log(totals);
 
 // Object litteral syntax
-const roger = {
-  firstName: "Roger",
-  lastName: "ODonnell",
-  age: 2025 - 2003,
-  job: "IT",
-  friends: ["Michael", "Steven", "Peter"],
-};
+// const roger = {
+//   firstName: "Roger",
+//   lastName: "ODonnell",
+//   age: 2025 - 2003,
+//   job: "IT",
+//   friends: ["Michael", "Steven", "Peter"],
+// };
 
-console.log(roger);
-console.log(roger.lastName);
-console.log(roger["lastName"]);
+// console.log(roger);
+// console.log(roger.lastName);
+// console.log(roger["lastName"]);
 
-const nameKey = "Name";
-console.log(roger["first" + nameKey]);
-console.log(roger["last" + nameKey]);
+// const nameKey = "Name";
+// console.log(roger["first" + nameKey]);
+// console.log(roger["last" + nameKey]);
 
-// const interestedIn = prompt(
-//   "What do you want to know about Roger?, Choose between firstName, lastName, age, job, friends"
+// // const interestedIn = prompt(
+// //   "What do you want to know about Roger?, Choose between firstName, lastName, age, job, friends"
+// // );
+
+// // if (roger[interestedIn]) {
+// //   console.log(roger[interestedIn]);
+// // } else {
+// //   prompt(
+// //     "Incorrect option: reselect from the following,Choose between firstName, lastName, age, job, friends"
+// //   );
+// // }
+
+// roger.location = "Ireland";
+// roger["codú"] = "codu rocks!";
+
+// console.log(roger.codú);
+// console.log(roger);
+
+// //Challenge
+// //Roger has 3 friends and his best friend is called michael
+
+// console.log(
+//   `${roger.firstName} has ${roger.friends.length} friends and his best friend is called ${roger.friends[0]}.`
 // );
 
-// if (roger[interestedIn]) {
-//   console.log(roger[interestedIn]);
-// } else {
-//   prompt(
-//     "Incorrect option: reselect from the following,Choose between firstName, lastName, age, job, friends"
+// const roger = {
+//   firstName: "Roger",
+//   lastName: "ODonnell",
+//   birthYear: 2003,
+//   job: "IT",
+//   friends: ["Michael", "Steven", "Peter"],
+//   hasDriversLicence: true,
+
+//   // calcAge: function (birthYear) {
+//   //   return 2025 - birthYear;
+//   // },
+//   // calcAge: function () {
+//   //   // console.log(this);
+//   //   return 2025 - this.birthYear;
+//   // },
+//   calcAge: function () {
+//     this.age = 2025 - this.birthYear;
+//     return this.age;
+//   },
+//   getSummary: function () {
+//     console.log(
+//       `${this.firstName} is a ${this.calcAge()} year old in ${
+//         this.job
+//       }, and he has ${this.hasDriversLicence ? "a" : "no"} drivers license`
+//     );
+//   },
+// };
+
+// console.log(roger.calcAge());
+
+// console.log(roger.age);
+// console.log(roger.age);
+// console.log(roger.age);
+
+// // console.log(roger["calcAge"](2003));
+
+// //challenge
+// //Roger is a 22 year old in it, and he has a drivers license.
+// //GetSummary - return personal data about roger
+
+// console.log(roger.getSummary());
+
+//####### CHALLENGE #3 ###############
+// Let's go back to Mark and John comparing their BMIs!
+
+// This time, let's use objects to implement the calculations! Remember: BMI = mass / (height * height) (mass in kg and height in meters).
+
+// Your tasks:
+
+// For each of them, create an object with properties for their full name, mass, and height (Mark Miller and John Smith). Name these objects as mark and john, and their properties exactly as fullName, mass and height.
+
+// Create a calcBMI method on each object to calculate the BMI (the same method on both objects). Assign the BMI value to a property called bmi (lowercase), and also return it from the method.
+
+// Log to the console who has the higher BMI, together with the full name and the respective BMI. Example: "John Smith's BMI (28.3) is higher than Mark Miller's (23.9)!".
+
+// TEST DATA: Marks weighs 78 kg and is 1.69 m tall. John weighs 92 kg and is 1.95 m tall.
+
+// const mark = {
+//   fullName: "Mark Miller",
+//   mass: 78,
+//   height: 1.69,
+//   calcBMI: function () {
+//     this.bmi = this.mass / this.height ** 2;
+//     return this.bmi;
+//   },
+// };
+// const john = {
+//   fullName: "John Smith",
+//   mass: 92,
+//   height: 1.95,
+//   calcBMI: function () {
+//     this.bmi = this.mass / this.height ** 2;
+//     return this.bmi;
+//   },
+// };
+
+// mark.calcBMI();
+// john.calcBMI();
+
+// if (mark.bmi > john.bmi) {
+//   console.log(
+//     `${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s (${john.bmi})!`
 //   );
+// } else if (john.bmi > mark.bmi) {
+//   console.log(
+//     `${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s (${mark.bmi})!`
+//   );
+// } else {
+//   console.log("BMI'S are the same");
 // }
-
-roger.location = "Ireland";
-roger["codú"] = "codu rocks!";
-
-console.log(roger.codú);
-console.log(roger);
-
-//Challenge
-//Roger has 3 friends and his best friend is called michael
-
-console.log(
-  `${roger.firstName} has ${roger.friends.length} friends and his best friend is called ${roger.friends[0]}.`
-);
