0b8bcedba54761c61057ab9bf68170ff323e5842
Roger O'Donnell <71945647+Roger-ODonnell@users.noreply.github.com>
2025-03-15 13:51:09 +0000

    Finished js foundations

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -401,23 +401,89 @@ function log(log) {
 // }
 
 
-const roger = [
-    'roger',
-    'odonnell',
-    2037 - 2003,
-    'it',
-    ['michael', 'peter', 'steven'],
-    true
-];
+// const roger = [
+//     'roger',
+//     'odonnell',
+//     2037 - 2003,
+//     'it',
+//     ['michael', 'peter', 'steven'],
+//     true
+// ];
 
 // for(let i = roger.length - 1; i >= 0; i--){
 //     console.log(i, roger[i]);
 // }
 
-for (let exercise  = 1; exercise < 4; exercise++){
-    console.log(`------------ Starting exercise ${exercise}`);
+// for (let exercise  = 1; exercise < 4; exercise++){
+//     console.log(`------------ Starting exercise ${exercise}`);
 
-    for (let rep = 1; rep < 6; rep++){
-        console.log(`Lifting weights repetition ${rep} 🏋️`);
-    }
-}
+//     for (let rep = 1; rep < 6; rep++){
+//         console.log(`Lifting weights repetition ${rep} 🏋️`);
+//     }
+// }
+
+// for (let rep = 1; rep <= 10; rep++){
+//     console.log(`Lifting weights reptition ${rep} 🏋️`);
+// }
+
+// let rep = 1;
+// while  ( rep <= 10){
+//     console.log(`Lifting weights reptition ${rep} 🏋️`);
+//     rep++;
+// }
+
+
+// let dice = Math.trunc(Math.random() * 6) + 1;
+
+// while (dice !== 6){
+//     console.log(` You rolled a ${dice}`);
+//     dice = Math.trunc(Math.random() * 6) + 1;
+//     if (dice === 6 ){
+//         console.log("loop is about to end...");
+//     }
+// }
+
+// CHALLENGE #4
+// Let's improve Steven's tip calculator even more, this time using loops!
+
+// Your tasks:
+
+// Create an array called bills containing all 10 test bill values.
+
+// Create empty arrays for the tips and the totals (tips and totals)
+
+// Use the calcTip function we wrote before (included in the starter code) to calculate tips and total values (bill + tip) for every bill value in the bills array. Use a for loop to perform the 10 calculations!
+
+
+
+// TEST DATA: 22, 295, 176, 440, 37, 105, 10, 1100, 86, and 52.
+
+
+
+// BONUS:
+
+// Write a function calcAverage which takes an array called arr as an argument. This function calculates the average of all numbers in the given array. This is a DIFFICULT challenge (we haven't done this before)! Here is how to solve it if you feel like it:
+
+// First, you will need to add up all values in the array. To do the addition, start by creating a variable sum that starts at 0. Then loop over the array using a for loop. In each iteration, add the current value to the sum variable. This way, by the end of the loop, you have all values added together.
+
+// To calculate the average, divide the sum you calculated before by the length of the array (because that's the number of elements).
+
+// Call the function with the totals array.
+
+
+
+// const bills = [1, 12, 23, 45, 50, 12,35,46,29,58];
+// const tips = [];
+// const totals = [];
+
+// const calcTip = function (bill) {
+//     return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+//   }
+
+// for (let i = 0; i <= bills.length - 1; i++){
+//     tips.push(Math.trunc(calcTip(bills[i])));
+//     totals.push(bills[i] + tips[i]);
+// }
+
+// console.log(`Your total tips are ${tips}`);
+// console.log(`Your totals are ${totals}`);
