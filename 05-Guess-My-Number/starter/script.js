e93d06de44eac6a4f3975724db931f068440b0c9
Roger O'Donnell <71945647+Roger-ODonnell@users.noreply.github.com>
2025-03-26 18:26:54 +0000

    Completed number guess game

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -9,11 +9,63 @@
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
 
+let secretNumber = RandomNumberOneToTwenty();
+let score = 20;
+let highScore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+function RandomNumberOneToTwenty() {
+  return Math.trunc(Math.random() * 20) + 1;
+}
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
+  //when there is no guess
   if (!guess) {
-    document.querySelector('.message').textContent = '⛔No Number!';
+    displayMessage('⛔No Number!');
+
+    //when guess is correct
+  } else if (guess === secretNumber) {
+    displayMessage(' 🎉 Correct Number!');
+    document.querySelector('.number').textContent = secretNumber;
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highScore) {
+      highScore = score;
+    }
+
+    document.querySelector('.highscore').textContent = highScore;
+
+    //when guess is too high
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? 'Too High! 📈' : 'Too Low! 📉');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('You lost the game! 💥');
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
+
+const resetButton = document
+  .querySelector('.again')
+  .addEventListener('click', function () {
+    displayMessage('Start guessing...');
+    document.querySelector('.score').textContent = '20';
+    document.querySelector('.guess').value = '';
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+    document.querySelector('.number').textContent = '?';
+    score = 20;
+    secretNumber = RandomNumberOneToTwenty();
+  });
